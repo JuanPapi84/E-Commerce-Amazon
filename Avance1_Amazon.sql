517502111c8fecc58190d69f9c6b5dae040a0995
Gina Correa <ginagiulianacorrea@gmail.com>
2024-05-22 13:34:24 -0300

    Add files via upload
    En este archivo de SQL trabaje en SQL Server Management Studio, en primer lugar importe el dataframe .csv, luego hice un análisis exploratorio y finalmente la creación de dos tablas.

---
+++ Avance1_Amazon.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{DA146C07-910E-4E7A-81C4-132D03804918}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {E811333D-3A9E-46D4-A88A-D2AC7E7B294D}
+	EndGlobalSection
+EndGlobal
